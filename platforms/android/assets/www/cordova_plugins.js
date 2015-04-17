5d28d5fc1a8d0aa63d58a5f2d9fffc9fdf03c63f
Hasta <tamanghasta@gmail.com>
2015-04-17 09:34:25 +0100

    location added

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -12,7 +12,8 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "org.apache.cordova.device": "0.3.0",
-    "org.apache.cordova.console": "0.2.13"
+    "org.apache.cordova.console": "0.2.13",
+    "org.apache.cordova.geolocation": "0.3.12"
 }
 // BOTTOM OF METADATA
 });
